e72274094f19d6f35e18661e79f7ac9e0a78f9bd
Lubna Fathima <lubnafathima275@gmail.com>
2024-10-04 13:30:57 +0530

    Feat: Updated Student's  menus

--- src/utils/Helpers.jsx
+++ src/utils/Helpers.jsx
@@ -185,6 +185,11 @@ export const MENU_LIST = {
       icon: <MdSettings className="w-6 h-6" />,
       path: "/students/communication",
     },
+    {
+      name: "Notifications",
+      icon: <MdSettings className="w-6 h-6" />,
+      path: "/students/notifications",
+    },
   ],
 
   parents: [
