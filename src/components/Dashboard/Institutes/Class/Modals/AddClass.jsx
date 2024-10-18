2af314b909b34a78e5ea9886c1ba3805852d86df
Lubna Fathima <lubnafathima275@gmail.com>
2024-10-19 00:36:00 +0530

    Added Institute's Class Menu

---
+++ src/components/Dashboard/Institutes/Class/Modals/AddClass.jsx
@@ -0,0 +1,92 @@
+import SubTitleComponent from "../../../../../styledComponents/SubTitleComponent";
+import ButtonFilled from "../../../../../styledComponents/ButtonFilled";
+import TableComponent from "../../../../../styledComponents/TableComponent";
+
+const tableHeader = ["Subject", "Subject Teacher", "Edit/Remove", "Action"];
+
+const tableData = [
+  ["Mathematics", "Mr. Smith", "Edit", "Delete"],
+  ["Science", "Mrs. Clark", "Edit", "Delete"],
+  ["History", "Mr. Brown", "Edit", "Delete"],
+];
+
+const AddClass = ({ onCancel }) => {
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  // };
+
+  return (
+    <form className="space-y-4">
+      <div className="max-h-96 overflow-y-auto scrollbar-custom p-4">
+        <div className="">
+          <label
+            className="block text-sm font-medium mb-1"
+            htmlFor="gradeLevel"
+          >
+            Grade Level
+          </label>
+          <input
+            type="text"
+            id="gradeLevel"
+            className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            placeholder="10"
+          />
+        </div>
+        <div className="mt-4">
+          <label className="block text-sm font-medium mb-1" htmlFor="section">
+            Section
+          </label>
+          <input
+            type="email"
+            id="section"
+            className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            placeholder="A"
+          />
+        </div>
+        <div className="mt-4">
+          <SubTitleComponent title="Assign Class Teacher" />
+          <input
+            type="number"
+            id="classTeacher"
+            className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-500"
+            placeholder="Dropdown list of available teachers"
+          />
+        </div>
+        <div className="mt-4">
+          <div className="flex justify-between items-center gap-2">
+            <SubTitleComponent title="Assign Subject Teacher" />
+            <ButtonFilled text="Add" />
+          </div>
+          <TableComponent tableHeader={tableHeader} tableData={tableData} />
+        </div>
+        <div className="mt-4">
+          <div className="flex justify-between items-center gap-2">
+            <SubTitleComponent title="Assign Student" />
+            <div className="flex gap-2">
+              <ButtonFilled text="Add" />
+              <ButtonFilled text="Import (excel/csv)" />
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="flex justify-end space-x-2">
+        <button
+          type="button"
+          onClick={onCancel}
+          className="bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-semibold py-2 px-4 rounded"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="bg-blue-600 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddClass;
--- src/pages/Dashboard/Institutes/Class.jsx
+++ src/pages/Dashboard/Institutes/Class.jsx
@@ -1,10 +1,55 @@
+import { useState } from "react";
+import InputComponent from "../../../styledComponents/InputComponent";
+import ButtonFilled from "../../../styledComponents/ButtonFilled";
+import TableComponent from "../../../styledComponents/TableComponent";
+import Modal from "../../../styledComponents/Modal";
+import AddClass from "../../../components/Dashboard/Institutes/Class/Modals/AddClass";
+
+const tableHeader = [
+  "Grade",
+  "Section",
+  "Class Teacher",
+  "Total Students",
+  "Subjects",
+  "Status (sort)",
+  "Actions",
+];
+
+const tableData = [
+  ["10th", "A", "Mrs. Johnson", "30", "6", "Active", "Edit/View"],
+  ["9th", "B", "Mr. Brown", "28", "5", "Active", "Edit/View"],
+  ["8th", "A", "Mrs. Smith", "25", "6", "Pending", "Assign"],
+  ["7th", "C", "Ms. White", "27", "4", "Active", "Edit/View"],
+];
 
 const Class = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const displayModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <div>
-      Institutes Class
+      <InputComponent placeholder="Search by Class Name or Teacher Name" />
+      <div className="mt-4 flex flex-wrap gap-2">
+        <ButtonFilled text="Add Class" onClick={() => displayModal()} />
+        <ButtonFilled text="Export (CSV / PDF)" />
+        <ButtonFilled text="Delete" />
+        <ButtonFilled text="Archive" />
+        <ButtonFilled text="Deactivate" />
+        <ButtonFilled text="Import (Bulk)" />
+      </div>
+      <TableComponent tableHeader={tableHeader} tableData={tableData} />
+      <Modal
+        showModal={showModal}
+        onClose={() => setShowModal(false)}
+        title="Add New Class"
+      >
+        <AddClass onCancel={() => setShowModal(false)} />
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default Class
+export default Class;
